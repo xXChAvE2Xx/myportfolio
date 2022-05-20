5013f235a9e460f9d26ad207526e3a0615f6ad06
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-05-20 18:25:17 -0500

    Se creo componente para ir al cv

---
+++ src/components/ShowCV.vue
@@ -0,0 +1,50 @@
+<template>
+  <a href="" target="blank_" class="button">
+    <span class="button-content">Ver CV</span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'verCV',
+};
+</script>
+<style scoped>
+.button {
+  position: relative;
+  overflow: hidden;
+  height: 3rem;
+  padding: 0 2rem;
+  border-radius: 1.5rem;
+  background: #3d3a4e;
+  background-size: 400%;
+  color: #fff;
+}
+
+.button:hover::before {
+  transform: scaleX(1);
+}
+
+.button-content {
+  position: relative;
+  z-index: 1;
+}
+
+.button::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: scaleX(0);
+  transform-origin: 0 50%;
+  width: 100%;
+  height: inherit;
+  border-radius: inherit;
+  background: linear-gradient(
+    82.3deg,
+    rgba(150, 93, 233, 1) 10.8%,
+    rgba(99, 88, 238, 1) 94.3%
+  );
+  transition: all 0.475s;
+}
+</style>
